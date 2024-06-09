bde9d49f7d2017df020ae5299c3e8fd0666e03b9
Virtek17 <129055919+Virtek17@users.noreply.github.com>
2024-06-10 01:09:08 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// .trim().toLowerCase();
+
+document.getElementById('alertButton').addEventListener('click', function() {
+   var userInput = document.getElementById('exampleFormControlTextarea1').value
+   var modalMessage = document.getElementById('modalMessage');
+   var modalImage = document.getElementById('modalImage');
+
+   if (userInput === 'Зигмунд Фрейд') {
+       modalImage.src = '../img/good.jpg'; // Укажите путь к изображению для "ок"
+       modalImage.style.display = 'block';
+   }
+      else if (userInput === 'Фрейд')  {
+         modalImage.src = '../img/pochti.png'; // Укажите путь к изображению для "ок"
+         modalImage.style.display = 'block';
+      }
+   else {
+       modalImage.src = '../img/bad.png'; // Укажите путь к изображению для "не ок"
+       modalImage.style.display = 'block';
+   }
+});
