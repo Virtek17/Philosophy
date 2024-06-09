67ca8448be179406204d0d079e7e45ecf6f29f55
Virtek17 <129055919+Virtek17@users.noreply.github.com>
2024-06-10 01:39:22 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,15 +6,15 @@ document.getElementById('alertButton').addEventListener('click', function() {
    var modalImage = document.getElementById('modalImage');
 
    if (userInput === 'Зигмунд Фрейд') {
-       modalImage.src = '../img/good.jpg'; // Укажите путь к изображению для "ок"
+       modalImage.src = './img/good.jpg'; // Укажите путь к изображению для "ок"
        modalImage.style.display = 'block';
    }
       else if (userInput === 'Фрейд')  {
-         modalImage.src = '../img/pochti.png'; // Укажите путь к изображению для "ок"
+         modalImage.src = './img/pochti.png'; // Укажите путь к изображению для "ок"
          modalImage.style.display = 'block';
       }
    else {
-       modalImage.src = '../img/bad.png'; // Укажите путь к изображению для "не ок"
+       modalImage.src = './img/bad.png'; // Укажите путь к изображению для "не ок"
        modalImage.style.display = 'block';
    }
 });
